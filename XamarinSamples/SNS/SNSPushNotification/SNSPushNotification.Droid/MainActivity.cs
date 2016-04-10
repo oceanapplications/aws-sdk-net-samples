89d0ce50fed1507721df5cae84543354f6691832
oceanapplications <oceanapplications@gmail.com>
2016-04-09 20:45:59 -0600

    Update MainActivity.cs
    Fixed indentation

--- XamarinSamples/SNS/SNSPushNotification/SNSPushNotification.Droid/MainActivity.cs
+++ XamarinSamples/SNS/SNSPushNotification/SNSPushNotification.Droid/MainActivity.cs
@@ -20,18 +20,18 @@ namespace SNSPushNotification.Droid
 			global::Xamarin.Forms.Forms.Init (this, bundle);
 			LoadApplication (new SNSPushNotification.App ());
 
-            RegisterForGCM();
-        }
+        		 RegisterForGCM();
+        	}
 
-        private void RegisterForGCM()
-        {
-            string senders = Constants.GoogleConsoleProjectId;
-            Intent intent = new Intent("com.google.android.c2dm.intent.REGISTER");
-            intent.SetPackage("com.google.android.gsf");
-            intent.PutExtra("app", PendingIntent.GetBroadcast(this, 0, new Intent(), 0));
-            intent.PutExtra("sender", senders);
-            StartService(intent);
-        }
-    }
+	        private void RegisterForGCM()
+	        {
+	            string senders = Constants.GoogleConsoleProjectId;
+	            Intent intent = new Intent("com.google.android.c2dm.intent.REGISTER");
+	            intent.SetPackage("com.google.android.gsf");
+	            intent.PutExtra("app", PendingIntent.GetBroadcast(this, 0, new Intent(), 0));
+	            intent.PutExtra("sender", senders);
+	            StartService(intent);
+	        }
+    	}
 }
 
